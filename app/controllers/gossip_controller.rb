6738c731fa66cc6567a473435361243f2ef9b4ef
0xKubitus <n.cubilier@gmail.com>
2022-05-11 21:12:51 +0200

    apres manger : ajouter la creation d'un gossip

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -11,7 +11,23 @@ class GossipController < ApplicationController
     @gossip = Gossip.new
   end
 
-  def create
+  def create # ici 'params' sert à récupérer les inputs de l'utilisateur dans le formulaire de la page 'gossip/new' et créé une entre en BDD avec ces données.
+    @gossip = Gossip.new( 
+      'title' => params[:title],
+      'content' => params[:content]  
+    )
+  
+    if @gossip.save # on tente de voir si la création a bien fonctionné :
+      puts " "
+      puts "    GG ! Ta rumeur enregistrée dans la BDD, champion !!!"
+      puts " "
+      redirect_to root_path # si c'est le cas, l'utilisateur doit être redirigé vers la page 'root' (ou "/")
+    else
+      puts " "
+      puts " Et non t'es une brèle, essaye encore !!!"
+      puts " "
+      render 'new'
+    end
 
   end
 
