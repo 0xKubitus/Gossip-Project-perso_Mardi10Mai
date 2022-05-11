bfaadc9a7ae7ac0c6f5bbf4af31d14edf1778d55
0xKubitus <n.cubilier@gmail.com>
2022-05-11 16:38:32 +0200

    page view gossip/new créée avec formulaire mais ne prend pas les user input en base

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :gossip
   root to: 'gossip#index' # défini le 'root_path' sur la view index du gossip controller (on arrive sur cette page lorsqu'on tape 'localhost:3000')
-  get '/gossip/:id', to: 'gossip#show', as: 'gossip' # défini 'localhost:3000/gossip/*id du gossip*' sur la view "gossip/show" gérée par la méthode show du GossipController - le 'as: 'gossip' permet de définir un préfix dans la route pour pouvoir faire un "link_to"
+  #get '/gossip/:id', to: 'gossip#show', as: 'show_gossip' # défini 'localhost:3000/gossip/*id du gossip*' sur la view "gossip/show" gérée par la méthode show du GossipController - le 'as: 'gossip' permet de définir un préfix dans la route pour pouvoir faire un "link_to"
 
   get '/contact', to: "static#contact"
   get '/team', to: "static#team"
   get '/welcome/:first_name', to: 'static#welcome'
-  
-  
+  #get '/gossip/new', to
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
